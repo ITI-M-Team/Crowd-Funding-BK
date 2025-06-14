4931dc24c4af813c809f25de5fcd100ad455f028
Fatma-mosaad <142949799+Fatma-mosaad@users.noreply.github.com>
2025-06-14 23:57:35 +0300

    add home project

--- Crowd-Funding-Web-app/crowd_funding/views.py
+++ Crowd-Funding-Web-app/crowd_funding/views.py
@@ -11,6 +11,10 @@ from django.conf import settings
 from django.contrib.auth import login
 from .models import User, EmailActivation, PasswordReset, Projects, Comment, Rating, Report, Donation
 from .serializers import *
+from django.db.models import Avg, Value, FloatField
+from django.utils.timezone import now
+from django.db.models.functions import Coalesce
+
 import uuid
 ### Try With Unautheticated
 from rest_framework.permissions import AllowAny
@@ -300,3 +304,26 @@ class UpdateUserProfileView(APIView):
             serializer.save()
             return Response({'message': 'Profile updated successfully.'})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def home_projects(request):
+    current_time = now()
+    base_qs = Projects.objects.filter(is_canceled=False)
+
+    latest_projects = base_qs.order_by('-created_at') \
+        .annotate(avg_rating=Coalesce(Avg('ratings__score'), Value(0, output_field=FloatField())))[:5]
+
+    featured_projects = base_qs.filter(is_featured=True).order_by('-created_at') \
+        .annotate(avg_rating=Coalesce(Avg('ratings__score'), Value(0, output_field=FloatField())))[:5]
+
+    top_rated_projects = base_qs.annotate(
+    avg_rating=Coalesce(Avg('ratings__score'), Value(0, output_field=FloatField()))
+    ).order_by('-avg_rating')[:5]
+   
+
+    return Response({
+        "latest_projects": ProjectSerializer(latest_projects, many=True, context={'request': request}).data,
+        "featured_projects": ProjectSerializer(featured_projects, many=True, context={'request': request}).data,
+        "top_rated_projects": ProjectSerializer(top_rated_projects, many=True, context={'request': request}).data,
+    })
